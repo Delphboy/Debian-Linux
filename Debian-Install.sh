bb9ef5f79b30e588d1bde0f50729aa9a87a1fe28
Henry <hps1945@live.co.uk>
2017-08-09 10:00:58 +0100

    Added Debian Installation script and ElementaryOS tweaks installation script

---
+++ Debian-Install.sh
@@ -0,0 +1,21 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install firefox
+sudo apt-get install git
+
+#Install VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+# Install Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+#Install Vim
+sudo apt-get install vim
+
+exit
---
+++ ElementaryOS-Install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update && sudo apt-get upgrade
+
+echo 'ElementaryOS Stuff'
+sudo apt install software-properties-common
+gsettings set org.gnome.mutter overlay-key "'Super_L'"
+gsettings set org.pantheon.desktop.gala.behavior overlay-action "'wingpanel --toggle-indicator=app-launcher'"
+sudo add-apt-repository ppa:philip.scott/elementary-tweaks && sudo apt-get update
+sudo apt-get install elementary-tweaks
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-get update
+sudo apt-get install papirus-icon-theme
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-icon-theme-circle
+sudo apt-install Downloads/osx-arc-collection_1.4.3_amd64.deb 
+sudo apt install Downloads/osx-arc-collection_1.4.3_amd64.deb 
+sudo apt install /home/henry/Downloads/osx-arc-collection_1.4.3_amd64.deb 
+exit
